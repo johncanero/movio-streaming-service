812b765696d78bbe3565f576a3c464a469d10286
John Canero <johncanero.official@gmail.com>
2023-07-13 18:35:51 +0800

    Align the text at the middle of the screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='bg-black py-96'>
-      <h1 className='text-2xl font-semibold text-center text-red-500'>Movio Streaming Service</h1>
+    <div className='flex items-center justify-center h-screen'>
+      <h1 className='text-2xl font-semibold text-red-500 '>Movio Streaming Service</h1>
     </div>
   )
 }
