2fbab84cdcd9cc4bccfdb8c280de5906d7c7ff21
John Canero <johncanero.official@gmail.com>
2023-07-15 23:27:17 +0800

    Add "use-client" to page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
+import { useCallback, useState } from 'react';
+
 import Input from '../components/Input'
 
 export default function Home() {
+    const [email, setEmail] = useState('');
+    
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-            <div className="bg-black w-full h-full lg:bg-opacity-50">
+            <div className="w-full h-full bg-black lg:bg-opacity-50">
 
                 {/* Stimeo Logo */}
                 <nav className="px-12 py-5">
@@ -14,15 +20,21 @@ export default function Home() {
                 
                 {/* Auth Screen UI */}
                 <div className="flex justify-center">
-                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                    <div className="self-center w-full px-16 py-16 mt-2 bg-black rounded-md bg-opacity-70 lg:w-2/5 lg:max-w-md">
                         {/* Auth Sign In */}
-                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                        <h2 className="mb-8 text-4xl font-semibold text-white">
                             Sign In
                         </h2>
                         
                         {/* Auth Input */}
                         <div className="flex flex-col gap-4">
-                            <Input />   
+                            <Input 
+                                id="email"
+                                type="email"
+                                label="Email address or phone number"
+                                value=""
+                                onChange={() => {}} 
+                             />   
                         </div>
                     </div>
                 </div>
