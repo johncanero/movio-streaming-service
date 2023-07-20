d0e0c2e04702bdd0c729dff3eb4bfdd7ba3ead84
John Canero <johncanero.official@gmail.com>
2023-07-20 17:31:21 +0800

    Add and update profile page content

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 import { NextPageContext } from "next";
 import { getSession, useSession } from "next-auth/react";
@@ -23,12 +24,28 @@ export async function getServerSideProps(context: NextPageContext) {
 
 const Profiles = () => {
     return ( 
-        <div className="flex items-center h-screen text-center">
-            <div className="m-auto">
-                <h2 className="mb-6 text-xl font-medium text-white">Profiles</h2>
-                <button onClick={() => signOut()} className="text-4xl text-white">
-                    <BsDoorOpen size={80} className='hover:text-neutral-600' />
-                </button>
+        <div className="flex items-center justify-center h-full">
+            <div className="flex flex-col">
+                
+                {/* Who's watching? */}
+                <h1 className="text-3xl text-center text-white md:text-6xl">Who&#39;s watching?</h1>
+
+                {/* Default Image */}
+                <div className="flex items-center justify-center gap-8 mt-10">
+                    <div onClick={() => {}}>
+
+                        <div className="flex-row mx-auto group w-44">
+                            <div className="flex items-center justify-center overflow-hidden border-2 border-transparent rounded-md w-44 h-44 group-hover:cursor-pointer group-hover:border-white">
+                                <img src="/images/default-blue.png" alt="Profile" />
+                            </div>
+
+                            <div className="mt-4 text-2xl text-center text-gray-400 group-hover:text-white">
+                                Name
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
      );
