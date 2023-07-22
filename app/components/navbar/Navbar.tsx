dd2549fb13b794fb14b4b3b7d4e1474246744110
John Canero <johncanero.official@gmail.com>
2023-07-23 00:40:07 +0800

    Add Navbar End Contents (ToggleAccountMenu)

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,15 +3,20 @@
 import { useState, useEffect, useCallback } from 'react';
 import Image from 'next/image';
 import { BsChevronDown } from 'react-icons/bs';
+import { BsSearch, BsBell } from 'react-icons/bs';
 
-import useMobileMenuModal from '@/app/hooks/useMobileMenuModal'; 
+import useMobileMenuModal from '@/app/hooks/useMobileMenuModal';
+import useMediaQuery from '@/app/hooks/useMediaQuery';
+
 import NavbarItem from './NavbarItem';
-import MobileMenu from '../MobileMenu';
 import MobileMenuModal from '../modals/MobileMenuModal';
+import MobileMenu from '../MobileMenu';
 
 const Navbar = () => {
+    const isLargeScreens = useMediaQuery('(max-width: 1023px)');
+    const [showAccountMenu, setShowAccountMenu] = useState(false);
     const [showBackground, setShowBackground] = useState(false);
-    const [showMobileMenu, setShowMobileMenu] = useState(false);
+    // const [showMobileMenu, setShowMobileMenu] = useState(false);
 
     const { onOpen } = useMobileMenuModal();
 
@@ -19,11 +24,15 @@ const Navbar = () => {
         onOpen();
     };
 
-    const toggleMobileMenu = useCallback(() => {
-        setShowMobileMenu((current) => !current);
+    // const toggleMobileMenu = useCallback(() => {
+    //     setShowMobileMenu((current) => !current);
+    // }, []);
+
+    const toggleAccountMenu = useCallback(() => {
+        setShowAccountMenu((current) => !current);
     }, []);
 
-    
+
     return (
         <nav className='fixed z-20 w-full'>
             <div className={`relative  transition duration-500 ${showBackground ? 'bg-zinc-900/90' : ''} h-28 md:h-auto`}>
@@ -69,10 +78,39 @@ const Navbar = () => {
                             <BsChevronDown size={15} className='text-white transition' />
                         </div>
                         {/* <MobileMenu visible={showMobileMenu} /> */}
-                        <MobileMenuModal />s
+                        <MobileMenuModal />
                     </div>
 
+                    {/* End of Navbar */}
+                    <div className='flex flex-row items-center ml-auto gap-7'>
+                        <div className='text-gray-200 transition cursor-pointer hover:opacity-70'>
+                            <BsSearch size={20} />
+                        </div>
+                        <div className='text-gray-200 transition cursor-pointer hover:opacity-70'>
+                            <BsBell size={20} />
+                        </div>
 
+                        <div
+                            onClick={toggleAccountMenu}
+                            className='relative flex flex-row items-center gap-3 cursor-pointer'
+                        >
+                            <div className='w-6 h-6 overflow-hidden rounded-md lg:w-10 lg:h-10'>
+                                <Image
+                                    src={'/images/default-red.png'}
+                                    alt='Profile'
+                                    height={500}
+                                    width={500}
+                                />
+                            </div>
+                            <BsChevronDown
+                                size={isLargeScreens ? 12 : 16}
+                                className={`text-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'
+                                    }`}
+                            />
+                            {/* <AccountMenu visible={showAccountMenu} /> */}
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </nav>
